6506c5ee505ba0e25b7ea837c88b4772b5e7c72e
Alby Chawula <albytchawula@iinet.net.au>
2015-01-11 13:37:55 +0800

    Added test functions for the Message class

--- tests/netcode_tests.py
+++ tests/netcode_tests.py
@@ -1,3 +1,13 @@
 from nose.tools import *
-import MyIRC
+from MyIRC.netcode import *
 
+import sys
+
+# Test functions for the netcode
+
+def test_message():
+	testmessage = Message("test message to send", 0)
+	
+	assert_equal(testmessage.type, 0) 
+	assert_equal(testmessage.message, "test message to send")
+	assert_equal(testmessage.length, sys.getsizeof(str(testmessage)))
