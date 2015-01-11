80a373558ce2eb5d6a29116ae2fc9c605dfcc677
Alby Chawula <albytchawula@iinet.net.au>
2015-01-11 13:07:24 +0800

    Recreated Socks class to manage socket communication separately from Server class. Server now contains a Socks object as an attribute and uses it to transmit and receive information from a server

--- MyIRC/netcode.py
+++ MyIRC/netcode.py
@@ -1,33 +1,94 @@
 import socket, sys
 
+
+# My basic socket object to handle connections between sockets
+class Socks(object):
+	def __init__(self, sock = None):
+		if sock is None:
+			self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		else:
+			self.sock = sock
+	
+	# Establishes a connection with the host and port 
+	def Connect(self, host, port):
+		self.sock.connect((host, port))
+		
+	# Disconnects the socket and closes it. CAN NO LONGER BE USED
+	def Disconnect(self):
+		self.sock.close()
+		
+	# Sends the message object to the connected socket
+	# This function assumes that the socket has already been connected
+	# using the Connect() function
+	def Send(self, message):
+		# Send the length
+		self.sock.send(str(message.length) + ":")
+	
+		# Send the message
+		totalsent = 0
+		while totalsent < message.length:
+			sent = self.sock.send(str(message))
+			if sent == 0:
+				raise RuntimeError("Connection broken")
+			totalsent = totalsent + sent
+	
 # Contains basic information about a server that the user is connected
 # to. Stores the socket and facilitates communication between the client
 # and the server.
+
+# Class relationships
+#	Has-a Socks (socket) object as an attribute
+#	Uses a message object to pass to the socket for transmission
+#
+# sock is not instantiated until actually connecting to the server.
 class Server(object):
 	
-	def __init__(self, name, address, port, s = None):
+	# Class attributes
+	instances = 0 # Keeps track of the number of servers currently instantiated
+	
+	def __init__(self, name, address, port, sock = None):
 		self.name = name
 		self.address = address
 		self.port = port
 		
-		if s is None:
-			self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		else:
-			self.s = s
-			
-	# Connects to a server using the server's address and port members
-	def ConnectToServer(self):
-		self.s.connect((self.address, self.port))
+		self.sock = sock
+		
+		instances += 1
 	
+	##################################################################
+	# The following methods use the Socks class 
+	##################################################################
+	
+	# Connects to and instantiates the server socket using the server's 
+	# address and port which are provided by the user.
+	#
+	# Note that a new Socks object is only created if Server does not 
+	# already have a Socks object
+	def ConnectToServer(self):
+		self.sock = Socks(self.sock) 
+		self.sock.Connect(self.address, self.port)
+	
+	# CLoses the socket and sets sock to None so if there are any 
+	# future connections with the Server object a new socket can be
+	# created
 	def DisconnectFromServer(self):
-		self.s.close()
+		self.sock.close()
+		self.sock = None
 		
 	def SendMessage(self, message):
 		self.s.send(str(message))
 
 	def ReceieveMessage(self):
-		return(self.s.recv(1024))
+		while bit != ":"
+		bit = self.s.recv(1)
 		
+	##################################################################
+# Class to store network messages
+
+# Message type indicates how the message should be handled by the client
+# Type 0 is meta data
+# Type 1 is voice data
+# Type 2 is text data		
 class Message(object):
 	
 	def __init__(self, message, type = 0):
@@ -36,4 +97,4 @@ class Message(object):
 		self.length = sys.getsizeof(type) + sys.getsizeof(message)
 		
 	def __str__(self):
-		return(str(self.length) + ":" + str(self.type) + ":" + self.message)
+		return(str(self.type) + ":" + self.message)
--- MyIRC/server.py
+++ MyIRC/server.py
@@ -1,5 +1,10 @@
 import socket
 
+
+
+clients = []
+
+
 serverSocket = socket.socket()
 
 host = socket.gethostname()
@@ -7,9 +12,15 @@ port = 12345
 serverSocket.bind((host, port))
 serverSocket.listen(5)
 
+# Receiving loop
+# Block until a connection is received and then receive data from 
+# the new client socket
 while True:
 	clientSocket, clientAddress = serverSocket.accept()
 	
 	data = clientSocket.recv(1024) # Receive up to 1024 bytes from the client
 	
 	print(data)
+	
+class Client(object):
+	
