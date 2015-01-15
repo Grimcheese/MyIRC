7dd6b85408fbca4b2911ffe342532e42a58c5db1
Alby Chawula <albytchawula@iinet.net.au>
2015-01-15 14:22:57 +0800

    Added MessageHandler to the server and support for a client connecting to the server.

--- MyIRC/server.py
+++ MyIRC/server.py
@@ -1,9 +1,9 @@
 import socket
 from netcode import Socks, Message, Server, BaseClient
 
-print("TEST SERVER IS NOW UP")
+print("TEST SERVER STARTING UP")
 
-clients = []
+clientList = []
 
 
 serverSocket = Socks()
@@ -13,20 +13,55 @@ port = 12345
 serverSocket.sock.bind((host, port))
 serverSocket.sock.listen(5)
 
+# Converts a message received via a socket as a string to an actual 
+# message object.
+def StringToMessageObject(messageString):
+	messageParts = messageString.split(Message.delimiter)
+	message = Message(tempMessage[0], tempMessage[1])
+	
+	return message
 
+# When the server receives a communication message from a client the
+# server will send that message to all the other clients that are 
+# connected to the server.
+def CommunicationMessageHandler(msg):
+	pass
+	"""for client in clientList:
+		
+		client.EstablishConnection()
+		client.SendMessage(msg)
+		client.CloseConnection()
+	"""
+def MetaMessageHandler(msg):
+	print(msg)
+	attributes = msg.message.split(" ")
+	if attributes[0] == "CONNECT":
+		newClient = Client(attributes[1], attributes[2], port)
+		clientList.append(newClient)
 
+def MessageHandler(msg):
+
+	if msg.GetType() == "META":
+		MetaMessageHandler(msg)
+	elif msg.GetType() == "VOICE" or msg.GetType() == "TEXT":
+		CommunicationMessageHandler(msg)
+	
 
 # Receiving loop
 # Block until a connection is received and then receive data from 
 # the new client socket
+
+print("Now accepting connections")
+
 while True:
 	clientSocket, clientAddress = serverSocket.sock.accept()
-	testClient = BaseClient("CLIENT", clientAddress[0], clientAddress[1], clientSocket)
+	tempClient = Client("TEMPCLIENT", clientAddress[0], clientAddress[1], clientSocket)
 	
-	data = testClient.ReceiveMessage()
+	msgString = tempClient.ReceiveMessage()
 	
 	# Message is handled
-	print(data)
+	clientMessage = StringToMessageObject(msgString)
+	MessageHandler(clientMessage)
 	
 	# Socket is destroyed
-	testClient.CloseConnection()
+	tempClient.CloseConnection()
